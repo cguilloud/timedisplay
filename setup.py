935fe673e0cfba4a3124db2bca8188b7abc051ec
CyrilGuilloud@pcsht <cyril.guilloud@esrf.fr>
2019-03-28 14:39:07 +0100

    prepare for packaging

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="timedisplay",
+    version="0.9.0",
+    description="Human-readable time durations printing library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.esrf.fr/cyril.guilloud/timedisplay",
+    author="Cyril Guilloud (ESRF-BCU)",
+    author_email="prenom.name@truc.fr",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: GPL3",
+        "Operating System :: OS Independent",
+    ],
+)
+
