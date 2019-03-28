51e19768ed1a5aacdf5172a45dd61ce98a79e4cf
CyrilGuilloud@pcsht <cyril.guilloud@esrf.fr>
2019-03-28 20:01:38 +0100

    moved code in sub directory

--- setup.py
+++ setup.py
@@ -3,20 +3,27 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
-    name="timedisplay",
-    version="0.9.0",
-    description="Human-readable time durations formating library",
-    long_description=long_description,
-    url="https://github.com/cguilloud/timedisplay",
-    author="Cyril Guilloud (ESRF-BCU)",
-    author_email="prenom.name@truc.fr",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 2.7",
+def setup_package():
+    setuptools.setup(
+        name="timedisplay",
+        version="0.9.0",
+        description="Human-readable time durations formating library",
+        long_description=long_description,
+        url="https://github.com/cguilloud/timedisplay",
+        keywords=['time', 'duration', 'human', 'formating'],
+        author="Cyril Guilloud (ESRF-BCU)",
+        author_email="prenom.name@truc.fr",
+        packages=['timedisplay'],
+        package_dir={'timedisplay': 'timedisplay'},
+        package_data={'timedisplay': ['test_timedisplay.py']},
+        classifiers=[
         "Programming Language :: Python :: 3",
         "License :: GPL3",
         "Operating System :: OS Independent",
-    ],
-)
+        ],
+        )
+
+
+if __name__ == "__main__":
+    setup_package()
 
